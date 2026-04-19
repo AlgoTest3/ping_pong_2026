a607d7503c4c6641353779ce405017356367e5a5
Anton_algo <167683642+AlgoTest3@users.noreply.github.com>
2026-04-19 16:12:03 +0300

    Update main.py
    Добавил классы GameSprite и Player

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, widht, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (widht, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if (keys[K_LEFT] or keys[K_a])and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if (keys[K_RIGHT] or keys[K_d]) and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
 back = (200, 255, 255)
 win_height = 500
 win_width = 600
