b74b1ec8587c402b91acfb39b5e817c0dfe11c53
Anton_algo <167683642+AlgoTest3@users.noreply.github.com>
2026-04-19 16:23:50 +0300

    Update main.py
    Добавил объект мяча

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ FPS = 60
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 while game:
     for e in event.get():
@@ -52,5 +53,6 @@ while game:
 
         racket1.reset()
         racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
