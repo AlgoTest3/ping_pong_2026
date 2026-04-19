83d331f373f86f44ab8619e2744e60a90951f8f6
Anton_algo <167683642+AlgoTest3@users.noreply.github.com>
2026-04-19 16:21:47 +0300

    Update main.py
    Добавил объекты ракеток, активировал их методы передвижения

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
 
 back = (200, 255, 255)
@@ -38,11 +38,19 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
     display.update()
     clock.tick(FPS)
